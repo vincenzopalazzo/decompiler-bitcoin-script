312779083ab257a4afce23f700da50d666d770f2
Vincenzo Palazzo <vincenzopalazzo1996@gmail.com>
2019-09-18 23:49:46 +0200

    Initial commit

---
+++ src/opcode.h
@@ -0,0 +1,8 @@
+//
+// Created by https://github.com/vincenzopalazzo on 9/18/19.
+//
+
+#ifndef DECOMPILE_BITCOIN_SCRIPT_OPCODE_H
+#define DECOMPILE_BITCOIN_SCRIPT_OPCODE_H
+
+#endif //DECOMPILE_BITCOIN_SCRIPT_OPCODE_H
